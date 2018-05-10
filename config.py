085ec8c96b2d5b21c4529eef59dbb1ac3721ca3d
dangerousor <1192350292@qq.com>
2018-05-10 19:46:41 +0800

    Add redis and register function

--- config.py
+++ config.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-import sys
-default_encoding = 'utf-8'
-if sys.getdefaultencoding() != default_encoding:
-    reload(sys)
-    sys.setdefaultencoding(default_encoding)
 
 USER = 'root'
-PASSWORD = '123456'
+PASSWORD = 'liuxiaofeng'
 URL = 'localhost'
 PORT = '3306'
-DATABASE = 'project'
-UPLOAD_PATH = 'users'
+DATABASE = 'android'
